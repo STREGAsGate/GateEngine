a5b859af9f43fb214557395465b2d9742364146b
Strega's Gate <STREGA@users.noreply.github.com>
2023-06-05 21:50:40 -0400

    Fix tvOS build error

--- Sources/GateEngine/System/Platforms/Platform.swift
+++ Sources/GateEngine/System/Platforms/Platform.swift
@@ -138,7 +138,7 @@ extension InternalPlatform {
         return url
     }
     
-    #if os(macOS) || os(iOS) || os(Windows) || os(Linux)
+    #if os(macOS) || os(iOS) || os(tvOS) || os(Windows) || os(Linux)
     func systemTime() -> Double {
         var time: timespec = timespec()
         if clock_gettime(CLOCK_MONOTONIC_RAW, &time) != 0 {
