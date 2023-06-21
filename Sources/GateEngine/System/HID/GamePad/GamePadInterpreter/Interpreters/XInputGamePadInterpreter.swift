5a42368c8c39edcb1ee7a0c7f0e3d8aea7217c92
Dustin Collins <STREGA@users.noreply.github.com>
2023-06-21 17:14:04 -0400

    Update XInputGamePadInterpreter.swift

--- Sources/GateEngine/System/HID/GamePad/GamePadInterpreter/Interpreters/XInputGamePadInterpreter.swift
+++ Sources/GateEngine/System/HID/GamePad/GamePadInterpreter/Interpreters/XInputGamePadInterpreter.swift
@@ -120,13 +120,19 @@ internal class XInputGamePadInterpreter: GamePadInterpreter {
         gamePad.shoulder.left.value = gamePad.shoulder.left.isPressed ? 1 : 0
 
         gamePad.shoulder.right.isPressed = buttons & XINPUT_GAMEPAD_RIGHT_SHOULDER != 0
-        gamePad.shoulder.right.value = gamePad.shoulder.right.isPressed ? 1 : 0
-
+        let RT_DEADZONE: Float = Float(XINPUT_GAMEPAD_TRIGGER_THRESHOLD)
+        let RT_RANGE: Float = 255.0 - RT_DEADZONE
+        let RT_VALUE: Float = Float(xGamePad.bLeftTrigger)
+        gamePad.trigger.right.value = RT_VALUE > RT_DEADZONE ? (RT_VALUE - RT_DEADZONE) / RT_RANGE : 0
+        
         gamePad.trigger.left.isPressed = xGamePad.bLeftTrigger > XINPUT_GAMEPAD_TRIGGER_THRESHOLD
-        gamePad.trigger.left.value = max(0, min(1, Float(xGamePad.bLeftTrigger) / 255))
+        let LT_DEADZONE: Float = Float(XINPUT_GAMEPAD_TRIGGER_THRESHOLD)
+        let LT_RANGE: Float = 255.0 - LT_DEADZONE
+        let LT_VALUE: Float = Float(xGamePad.bLeftTrigger)
+        gamePad.trigger.left.value = LT_VALUE > LT_DEADZONE ? (LT_VALUE - LT_DEADZONE) / LT_RANGE : 0
 
         gamePad.trigger.right.isPressed = xGamePad.bRightTrigger > XINPUT_GAMEPAD_TRIGGER_THRESHOLD
-        gamePad.trigger.right.value = max(0, min(1, Float(xGamePad.bRightTrigger) / 255))
+        gamePad.trigger.right.value = Float(xGamePad.bRightTrigger) / 255.0
 
         gamePad.menu.primary.isPressed = buttons & XINPUT_GAMEPAD_START != 0
         gamePad.menu.primary.value = gamePad.menu.primary.isPressed ? 1 : 0
@@ -134,13 +140,21 @@ internal class XInputGamePadInterpreter: GamePadInterpreter {
         gamePad.menu.secondary.isPressed = buttons & XINPUT_GAMEPAD_BACK != 0
         gamePad.menu.secondary.value = gamePad.menu.secondary.isPressed ? 1 : 0
 
-        gamePad.stick.left.xAxis = max(-1, min(1, Float(xGamePad.sThumbLX) / 32767))
-        gamePad.stick.left.yAxis = max(-1, min(1, Float(xGamePad.sThumbLY) / 32767))
+        let L_DEADZONE: Float = Float(XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE)
+        let L_RANGE: Float = 32767.0 - L_DEADZONE
+        let LX_VALUE: Float = Float(xGamePad.sThumbLX)
+        let LY_VALUE: Float = Float(xGamePad.sThumbLY)
+        gamePad.stick.left.xAxis = abs(LX_VALUE) > L_DEADZONE ? LX_VALUE < 0 ? (LX_VALUE + L_DEADZONE) / L_RANGE : (LX_VALUE - L_DEADZONE) / L_RANGE : 0
+        gamePad.stick.left.yAxis = abs(LY_VALUE) > L_DEADZONE ? LY_VALUE < 0 ? (LY_VALUE + L_DEADZONE) / L_RANGE : (LY_VALUE - L_DEADZONE) / L_RANGE : 0
         gamePad.stick.left.button.isPressed = buttons & XINPUT_GAMEPAD_LEFT_THUMB != 0
         gamePad.stick.left.button.value = gamePad.stick.left.button.isPressed ? 1 : 0
 
-        gamePad.stick.right.xAxis = max(-1, min(1, Float(xGamePad.sThumbRX) / 32767))
-        gamePad.stick.right.yAxis = max(-1, min(1, Float(xGamePad.sThumbRY) / 32767))
+        let R_DEADZONE: Float = Float(XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE)
+        let R_RANGE: Float = 32767.0
+        let RX_VALUE: Float = Float(xGamePad.sThumbRX)
+        let RY_VALUE: Float = Float(xGamePad.sThumbRY)
+        gamePad.stick.right.xAxis = abs(RX_VALUE) > R_DEADZONE ? RX_VALUE / R_RANGE : 0
+        gamePad.stick.right.yAxis = abs(RY_VALUE) > R_DEADZONE ? RY_VALUE / R_RANGE : 0
         gamePad.stick.right.button.isPressed = buttons & XINPUT_GAMEPAD_RIGHT_THUMB != 0
         gamePad.stick.right.button.value = gamePad.stick.right.button.isPressed ? 1 : 0
     }
