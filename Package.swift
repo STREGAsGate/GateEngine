a91ae117090389c3ea0dab6244ae99ee527b3a3a
STREGA <STREGA@users.noreply.github.com>
2023-06-24 20:39:12 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -98,7 +98,7 @@ let package = Package(
                             .define("GATEENGINE_PLATFORM_DEFERS_LAUNCH", .when(platforms: [.wasi])),
                         ])
                         
-                        #if true // Options for development of GateEngine. These should be commented out for tagged version releases.
+                        #if false // Options for development of GateEngine. These should be commented out for tagged version releases.
                         #warning("GateEngine development options are enabled. These can cause strange build errors on some platforms.")
                         
                         // Options for developemnt of WASI platform
