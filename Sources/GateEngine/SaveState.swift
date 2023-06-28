df7edc521d5c991cb12f9f7dd994b3d43c6dc029
Strega's Gate <STREGA@users.noreply.github.com>
2023-06-28 03:42:25 -0400

    Update SaveState.swift

--- Sources/GateEngine/SaveState.swift
+++ Sources/GateEngine/SaveState.swift
@@ -11,12 +11,14 @@ import GameMath
 /**
  Recommended values for game state storage conform to this protocol.
  
- It is not recommended, but you may add this prototcol to other vlaue types.
+ It is not recommended, but you may add this protocol to other vlaue types.
  The value types chosen were chosen for long term, cross platform, and cloud service support.
  */
+
 public protocol StateIntegerValue: BinaryInteger {}
 extension Int: StateIntegerValue {}
-extension UInt: StateIntegerValue {}
+// UInt won't fit in the integer storge
+// extension UInt: StateIntegerValue {}
 extension Int8: StateIntegerValue {}
 extension UInt8: StateIntegerValue {}
 extension Int16: StateIntegerValue {}
@@ -24,12 +26,13 @@ extension UInt16: StateIntegerValue {}
 extension Int32: StateIntegerValue {}
 extension UInt32: StateIntegerValue {}
 extension Int64: StateIntegerValue {}
-extension UInt64: StateIntegerValue {}
+// UInt64 won't fit in the integer storge
+// extension UInt64: StateIntegerValue {}
 
 /**
  Recommended values for game state storage conform to this protocol.
  
- It is not recommended, but you may add this prototcol to other vlaue types.
+ It is not recommended, but you may add this protocol to other vlaue types.
  The value types chosen were chosen for long term, cross platform, and cloud service support.
  */
 public protocol StateFloatingPointValue: BinaryFloatingPoint {}
@@ -39,51 +42,89 @@ extension Double: StateFloatingPointValue {}
 /**
  Recommended values for game state storage conform to this protocol.
  
- It is not recommended, but you may add this prototcol to other vlaue types.
+ It is not recommended, but you may add this protocol to other vlaue types.
  The value types chosen were chosen for long term, cross platform, and cloud service support.
  */
 public protocol StateCodableValue: Codable {}
+extension Color: StateCodableValue {}
+extension Insets: StateCodableValue {}
+extension Circle: StateCodableValue {}
+extension Rect: StateCodableValue {}
 extension Degrees: StateCodableValue {}
 extension Radians: StateCodableValue {}
+extension Matrix3x3: StateCodableValue {}
+extension Matrix4x4: StateCodableValue {}
 extension Size2: StateCodableValue {}
 extension Position2: StateCodableValue {}
 extension Direction2: StateCodableValue {}
 extension Size3: StateCodableValue {}
+extension Transform2: StateCodableValue {}
 extension Position3: StateCodableValue {}
 extension Direction3: StateCodableValue {}
 extension Quaternion: StateCodableValue {}
-extension Matrix3x3: StateCodableValue {}
-extension Matrix4x4: StateCodableValue {}
+extension Transform3: StateCodableValue {}
 
 extension Game {
     public class State: Codable {
         let encoder = JSONEncoder()
         let decoder = JSONDecoder()
 
-        enum CodingKeys: CodingKey {
+        enum CodingKeys: String, CodingKey {
             case bools
             case integers
             case doubles
             case strings
+            case datas
         }
         private var bools: [String : Bool]
         private var integers: [String : Int64]
         private var doubles: [String : Double]
         private var strings: [String : String]
+        private var datas: [String : Data]
         
+        /**
+         Add a bool value to the state.
+         - parameter key: A unique identifier that will be used to retrieve the value latyer.
+         - parameter value: The value to place in the state.
+         */
         public func setValue(_ value: Bool, forKey key: String) {
+            //assert(bools.keys.contains(key) == false, "Key already used for a bool value.")
+            assert(integers.keys.contains(key) == false, "Key already used for an integer value.")
+            assert(doubles.keys.contains(key) == false, "Key already used for a floating point value.")
+            assert(strings.keys.contains(key) == false, "Key already used for a string value.")
+            assert(datas.keys.contains(key) == false, "Key already used for an encoded value.")
             assert(key.isAscii, "ASCII characters are required for state keys for compatibility reasons.")
             bools.updateValue(value, forKey: key)
         }
+        /**
+         Retrieve an exisiting bool value from the state.
+         - parameter key: The unique identifier originally used to set the value.
+         - returns: The saved value or nil if no value was found.
+         */
         public func boolForKey(_ key: String) -> Bool {
             return bools[key] == true
         }
         
+        /**
+         Add a integer value to the state.
+         - parameter key: A unique identifier that will be used to retrieve the value latyer.
+         - parameter value: The value to place in the state.
+         */
         public func setValue<T: StateIntegerValue>(_ value: T, forKey key: String) {
+            assert(bools.keys.contains(key) == false, "Key already used for a bool value.")
+            //assert(integers.keys.contains(key) == false, "Key already used for an integer value.")
+            assert(doubles.keys.contains(key) == false, "Key already used for a floating point value.")
+            assert(strings.keys.contains(key) == false, "Key already used for a string value.")
+            assert(datas.keys.contains(key) == false, "Key already used for an encoded value.")
             assert(key.isAscii, "ASCII characters are required for state keys for compatibility reasons.")
             assert(MemoryLayout<T>.size <= MemoryLayout<Int64>.size, "\(T.self) is not guaranteed to fit in state storage and cannot be used.")
             integers.updateValue(Int64(value), forKey: key)
         }
+        /**
+         Retrieve an exisiting integer value from the state.
+         - parameter key: The unique identifier originally used to set the value.
+         - returns: The saved value or nil if no value was found.
+         */
         public func integerForKey<T: StateIntegerValue>(_ key: String, ofType: T.Type = Int.self) -> T? {
             if let int = integers[key] {
                 return T(int)
@@ -91,11 +132,26 @@ extension Game {
             return nil
         }
         
+        /**
+         Add a floating point value to the state.
+         - parameter key: A unique identifier that will be used to retrieve the value latyer.
+         - parameter value: The value to place in the state.
+         */
         public func setValue<T: StateFloatingPointValue>(_ value: T, forKey key: String) {
+            assert(bools.keys.contains(key) == false, "Key already used for a bool value.")
+            assert(integers.keys.contains(key) == false, "Key already used for an integer value.")
+            //assert(doubles.keys.contains(key) == false, "Key already used for a floating point value.")
+            assert(strings.keys.contains(key) == false, "Key already used for a string value.")
+            assert(datas.keys.contains(key) == false, "Key already used for an encoded value.")
             assert(key.isAscii, "ASCII characters are required for state keys for compatibility reasons.")
             assert(MemoryLayout<T>.size <= MemoryLayout<Double>.size, "\(T.self) is not guaranteed to fit in state storage and cannot be used.")
             doubles.updateValue(Double(value), forKey: key)
         }
+        /**
+         Retrieve an exisiting floating point value from the state.
+         - parameter key: The unique identifier originally used to set the value.
+         - returns: The saved value or nil if no value was found.
+         */
         public func floatForKey<T: StateFloatingPointValue>(_ key: String, ofType: T.Type = Float.self) -> T? {
             if let double = doubles[key] {
                 return T(double)
@@ -103,34 +159,72 @@ extension Game {
             return nil
         }
         
+        /**
+         Add a string value to the state.
+         - parameter key: A unique identifier that will be used to retrieve the value latyer.
+         - parameter value: The string to place in the state.
+         - note: The value has a length limit of 2048. If you can't know the length of the string saving it here is not wise.
+         */
         public func setValue(_ value: String, forKey key: String) {
+            assert(bools.keys.contains(key) == false, "Key already used for a bool value.")
+            assert(integers.keys.contains(key) == false, "Key already used for an integer value.")
+            assert(doubles.keys.contains(key) == false, "Key already used for a floating point value.")
+            //assert(strings.keys.contains(key) == false, "Key already used for a string value.")
+            assert(datas.keys.contains(key) == false, "Key already used for an encoded value.")
             assert(key.isAscii, "ASCII characters are required for state keys for compatibility reasons.")
+            assert(value.count <= 2048, "The value has a length of \(value.count) which exceeds maximum of 2048.")
             strings.updateValue(value, forKey: key)
         }
+        /**
+         Retrieve an exisiting string value from the state.
+         - parameter key: The unique identifier originally used to set the value.
+         - returns: The saved value or nil if no value was found.
+         */
         public func stringForKey(_ key: String) -> String? {
             return strings[key]
         }
         
+        /**
+            Encode a struct type.
+         
+            Valid types are GameMath values like ``Position3``, ``Color``, ``Degrees``, ``Rect``, etc...
+         */
         public func encode<T: StateCodableValue>(_ value: T, forKey key: String) throws {
+            assert(bools.keys.contains(key) == false, "Key already used for a bool value.")
+            assert(integers.keys.contains(key) == false, "Key already used for an integer value.")
+            assert(doubles.keys.contains(key) == false, "Key already used for a floating point value.")
+            assert(strings.keys.contains(key) == false, "Key already used for a string value.")
+            //assert(datas.keys.contains(key) == false, "Key already used for an encoded value.")
             assert(key.isAscii, "ASCII characters are required for state keys for compatibility reasons.")
             let data = try encoder.encode(value)
-            let string = data.base64EncodedString()
-            self.setValue(string, forKey: key)
+            datas.updateValue(data, forKey: key)
         }
+        /**
+         Decode a struct type.
+         
+         Valid types are GameMath values like ``Position3``, ``Color``, ``Degrees``, ``Rect``, etc...
+         - parameter type: The type top decode
+         - parameter key: The unique identifier originally used to set the value.
+         - returns: The saved value. or nil if no value was found.
+         - throws: Forwarded Swift.JSONDecoder errors if the decoding fails.
+         */
         public func decode<T: StateCodableValue>(_ type: T.Type, forKey key: String) throws -> T? {
-            if let string = self.stringForKey(key) {
-                if let data = Data(base64Encoded: string) {
-                    return try decoder.decode(type, from: data)
-                }
+            if let data = self.datas[key] {
+                return try decoder.decode(type, from: data)
             }
             return nil
         }
         
+        /**
+         Remove a value from the state
+         - parameter key: The key used to store the value.
+         */
         public func removeValueForKey(_ key: String) {
             bools.removeValue(forKey: key)
             integers.removeValue(forKey: key)
             doubles.removeValue(forKey: key)
             strings.removeValue(forKey: key)
+            datas.removeValue(forKey: key)
         }
 
         internal init() {
@@ -138,8 +232,13 @@ extension Game {
             self.integers = [:]
             self.doubles = [:]
             self.strings = [:]
+            self.datas = [:]
         }
-        var name: String! = nil
+        
+        /// The name used to save and load this state instance
+        internal var name: String! = nil
+        
+        /// Persist the current values which will be loaded next time the game is run
         @MainActor public func save() throws {
             try Game.shared.platform.saveState(self, as: name)
         }
