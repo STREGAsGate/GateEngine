561e9fdf855563d813c7bfd182d7a00c268104f0
STREGA <STREGA@users.noreply.github.com>
2023-12-08 15:33:56 -0500

    Forward size hint to spriteSheet texture

--- Sources/GateEngine/Resources/Sprites/SpriteSheet.swift
+++ Sources/GateEngine/Resources/Sprites/SpriteSheet.swift
@@ -41,12 +41,13 @@ import GameMath
 
     public convenience init(
         path: String,
+        sizeHint: Size2? = nil,
         mipMapping: MipMapping = .none,
         options: TextureImporterOptions = .none,
         sampleFilter: Sprite.SampleFilter = .nearest,
         tintColor: Color = .white
     ) {
-        let texture = Texture(path: path, mipMapping: mipMapping, options: options)
+        let texture = Texture(path: path, sizeHint: sizeHint, mipMapping: mipMapping, options: options)
         self.init(texture: texture, sampleFilter: sampleFilter, tintColor: tintColor)
     }
 
