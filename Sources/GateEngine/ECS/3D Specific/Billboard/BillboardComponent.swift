79e450d375495a2900d17e940e48b7a8e0cceda2
STREGA <STREGA@users.noreply.github.com>
2023-12-08 15:33:34 -0500

    Add billboarding

---
+++ Sources/GateEngine/ECS/3D Specific/Billboard/BillboardComponent.swift
@@ -0,0 +1,19 @@
+/*
+ * Copyright © 2023 Dustin Collins (Strega's Gate)
+ * All Rights Reserved.
+ *
+ * http://stregasgate.com
+ */
+
+public struct BillboardComponent: Component {
+    public enum Style {
+        /// Rotates to align with the plane on the camera
+        case align
+        /// Rotates to face toward the camera
+        case lookAt(constraint: Quaternion.LookAtConstraint)
+    }
+    public var style: Style = .align
+    
+    public init() {}
+    public static let componentID = ComponentID()
+}
---
+++ Sources/GateEngine/ECS/3D Specific/Billboard/BillboardSystem.swift
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2023 Dustin Collins (Strega's Gate)
+ * All Rights Reserved.
+ *
+ * http://stregasgate.com
+ */
+
+public final class BillboardSystem: System {
+    public override func update(game: Game, input: HID, withTimePassed deltaTime: Float) async {
+        guard let camera = game.cameraEntity else {return}
+        let cameraTransform = camera.transform3
+        
+        for entity in game.entities {
+            guard let billboardComponent = entity.component(ofType: BillboardComponent.self) else {continue}
+            guard let transformComponent = entity.component(ofType: Transform3Component.self) else {continue}
+            
+            switch billboardComponent.style {
+            case .align:
+                transformComponent.rotation = cameraTransform.rotation
+            case .lookAt(let constraint):
+                transformComponent.rotation = Quaternion(
+                    lookingAt: cameraTransform.position, 
+                    from: transformComponent.position,
+                    constraint: constraint,
+                    isCamera: false
+                )
+            }
+        }
+    }
+    
+    public override class var phase: System.Phase { .simulation }
+    public override nonisolated class func sortOrder() -> SystemSortOrder? { .last }
+}
