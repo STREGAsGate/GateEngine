36ed171f3dd9b74b54f25d72afb62fa366fd6a1c
STREGA <STREGA@users.noreply.github.com>
2024-06-23 13:10:18 -0400

    Adjust frame length calculation

--- Sources/GateEngine/ECS/2D Specific/Sprite/SpriteComponent.swift
+++ Sources/GateEngine/ECS/2D Specific/Sprite/SpriteComponent.swift
@@ -116,7 +116,7 @@ public final class SpriteComponent: Component {
             let startFrame = (animation.spriteSheetStart.y * columns) + animation.spriteSheetStart.x
             let endFrame = {
                 if let frameCount = animation.frameCount {
-                    return startFrame + (frameCount - 1)
+                    return startFrame + frameCount
                 }
                 let framesInAnimation = (columns * rows) - startFrame
                 return framesInAnimation
