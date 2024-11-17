66e26c941b3717e16849e8ebbbe218d6cf30c0eb
STREGA <STREGA@users.noreply.github.com>
2024-11-16 20:51:34 -0500

    Fix implementation

--- Sources/GameMath/2D Types/2D Physics/Line2D.swift
+++ Sources/GameMath/2D Types/2D Physics/Line2D.swift
@@ -17,14 +17,14 @@ public struct Line2D: Sendable {
 
 public extension Line2D {
     func pointNear(_ p: Position2) -> Position2 {
-        // get dot product of e1, e2
-        let e1 = Position2(p2.x - p1.x, p2.y - p1.y)
-        let e2 = Position2(p.x - p1.x, p.y - p1.y)
-        let valDp = e1.dot(e2)
-        // get squared length of e1
-        let len2 = e1.squaredLength
-        return Position2((p1.x + (valDp * e1.x) / len2),
-                         (p1.y + (valDp * e1.y) / len2))
+        let ab = p2 - p1
+        let ap = p - p1
+        
+        var t = ap.dot(ab) / ab.squaredLength
+        if t < 0 {t = 0}
+        if t > 1 {t = 1}
+
+        return p1 + (ab * t)
     }
     
     // TODO: This needs testing
