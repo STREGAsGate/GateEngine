2932df79bcbddbbfb3507a7f80ca5fc9ce8b40e7
STREGA <STREGA@users.noreply.github.com>
2025-11-10 18:18:19 -0500

    Merge Toggle into Button

--- Sources/GateEngine/UI/Button.swift
+++ Sources/GateEngine/UI/Button.swift
@@ -36,6 +36,39 @@ open class Button: Control {
         }
     }
     
+    public enum Kind {
+        case momentaryPush
+        case toggle
+    }
+    public let kind: Kind
+    
+    public var _value: Bool = false
+    public var value: Bool {
+        get {
+            switch self.kind {
+            case .momentaryPush:
+                return self.state == .selected
+            case .toggle:
+                return _value
+            }
+        }
+        set {
+            switch self.kind {
+            case .momentaryPush:
+                break
+            case .toggle:
+                self._value = newValue
+                if self.state != .highlighted {
+                    if self._value {
+                        self.setState(.selected, sendActions: false)
+                    }else{
+                        self.setState(.normal, sendActions: false)
+                    }
+                }
+            }
+        }
+    }
+    
     open override func canBeHit() -> Bool {
         return true
     }
@@ -45,27 +78,29 @@ open class Button: Control {
         public var rawValue: RawValue
         
         public static let pressed = Self(rawValue: 1 << 0)
+        public static let stateChanged = Self(rawValue: 1 << 1)
         
         public init(rawValue: RawValue) {
             self.rawValue = rawValue
         }
     }
     
-    private var eventActionStorage: [Event: [(Button)->()]] = [:]
+    private var eventActionStorage: [Event: [(_ button: Button)->()]] = [:]
     
     public final func sendActions(forEvent event: Event) {
-        if let eventActionStorage = eventActionStorage[event] {
-            weak let unownedSelf: Button! = self
+        if let eventActionStorage = self.eventActionStorage[event] {
             for block in eventActionStorage {
-                block(unownedSelf)
+                Task(priority: .userInitiated) { @MainActor in
+                    block(self)
+                }
             }
         }
     }
     
-    public final func action(completion: @escaping (Button)->()) {
-        var array = eventActionStorage[.pressed] ?? []
+    public final func addAction(forEvent event: Event, completion: @escaping (_ button: Button)->()) {
+        var array = eventActionStorage[event] ?? []
         array.append(completion)
-        eventActionStorage[.pressed] = array
+        eventActionStorage[event] = array
     }
     
     public enum State {
@@ -73,20 +108,37 @@ open class Button: Control {
         case highlighted
         case selected
     }
-    public var state: State = .normal {
-        didSet {
-            if state != oldValue {
-                self.stateDidChange()
-                if let color = backgroundColors[state] {
-                    self.backgroundColor = color
-                }
-                if labelCreated {
-                    if let color = textColors[state] {
-                        self.label.textColor = color
-                    }
-                }
+    private var _state: State = .normal
+    public var state: State {
+        get {
+            return _state
+        }
+        set {
+            self.setState(newValue, sendActions: false)
+        }
+    }
+    
+    private func setState(_ state: State, sendActions: Bool = true) {
+        let previousState = self._state
+        self._state = state
+        if previousState != state {
+            self._stateDidChange(sendActions: sendActions)
+        }
+    }
+    
+    final func _stateDidChange(sendActions: Bool) {
+        if let color = backgroundColors[state] {
+            self.backgroundColor = color
+        }
+        if labelCreated {
+            if let color = textColors[state] {
+                self.label.textColor = color
             }
         }
+        if sendActions {
+            self.sendActions(forEvent: .stateChanged)
+        }
+        self.stateDidChange()
     }
     
     open func stateDidChange() {
@@ -94,58 +146,119 @@ open class Button: Control {
     }
     
     open override func cursorEntered(_ cursor: Mouse) {
-        self.state = .highlighted
+        self.setState(.highlighted)
     }
     
     open override func cursorExited(_ cursor: Mouse) {
-        self.state = .normal
+        switch self.kind {
+        case .momentaryPush:
+            self.setState(.normal)
+        case .toggle:
+            if value {
+                self.setState(.selected)
+            }else{
+                self.setState(.normal)
+            }
+        }
     }
     
     open override func cursorButtonDown(button: MouseButton, mouse: Mouse) {
+        self.setState(.selected)
         if let mouseLocation = mouse.locationInView(self) {
             if self.bounds.contains(mouseLocation) {
+                switch self.kind {
+                case .momentaryPush:
+                    break
+                case .toggle:
+                    self._value.toggle()
+                }
                 self.sendActions(forEvent: .pressed)
             }
         }
-        self.state = .selected
     }
     
     open override func cursorButtonUp(button: MouseButton, mouse: Mouse) {
         if mouse.isInsideView(self) {
-            self.state = .highlighted
+            self.setState(.highlighted)
         }else{
-            self.state = .normal
-        }
-    }
-    
-    open override func touchesBegan(_ touches: Set<Touch>) {
-        self.state = .selected
-    }
-    
-    open override func touchesMoved(_ touches: Set<Touch>) {
-        self.state = .normal
-        for touch in touches {
-            if touch.isInsideView(self) {
-                self.state = .selected
+            switch kind {
+            case .momentaryPush:
+                self.setState(.normal)
+            case .toggle:
+                if _value {
+                    self.setState(.selected)
+                }else{
+                    self.setState(.normal)
+                }
             }
         }
     }
     
-    open override func touchesEnded(_ touches: Set<Touch>) {
-        self.state = .normal
+    open override func touchesBegan(_ touches: Set<Touch>) {
+        self.setState(.selected)
+    }
+    
+    open override func touchesMoved(_ touches: Set<Touch>) {
+        var nextState: State = .normal
+        switch self.kind {
+        case .momentaryPush:
+            break
+        case .toggle:
+            if _value {
+                nextState = .selected
+            }else{
+                nextState = .normal
+            }
+        }
+        
         for touch in touches {
             if touch.isInsideView(self) {
+                nextState = .selected
+            }
+        }
+        self.setState(nextState)
+    }
+    
+    open override func touchesEnded(_ touches: Set<Touch>) {
+        for touch in touches {
+            if touch.isInsideView(self) {
+                switch self.kind {
+                case .momentaryPush:
+                    break
+                case .toggle:
+                    self._value.toggle()
+                }
                 self.sendActions(forEvent: .pressed)
                 break
+            }
+        }
+        switch self.kind {
+        case .momentaryPush:
+            self.setState(.normal)
+        case .toggle:
+            if _value {
+                self.setState(.selected)
+            }else{
+                self.setState(.normal)
             }
         }
     }
     
     open override func touchesCanceled(_ touches: Set<Touch>) {
-        self.state = .normal
+        switch kind {
+        case .momentaryPush:
+            self.setState(.normal)
+        case .toggle:
+            if _value {
+                self.setState(.selected)
+            }else{
+                self.setState(.normal)
+            }
+        }
     }
     
-    public init(size: Size2? = nil, label: String? = nil, textColor: Color = .white, backgroundColor: Color = .blue, cornorRadius: Float? = nil, action: ((Button)->())? = nil) {
+    public init(kind: Kind = .momentaryPush, size: Size2? = nil, label: String? = nil, textColor: Color = .white, backgroundColor: Color = .blue, cornorRadius: Float? = nil, action: ((_ button: Button)->())? = nil) {
+        self.kind = kind
         super.init()
         
         self.clipToBounds = true
@@ -166,20 +279,20 @@ open class Button: Control {
             self.label.textColor = textColor
         }
         if let action {
-            self.eventActionStorage[.pressed] = [action]
+            self.addAction(forEvent: .pressed, completion: action)
         }
         
         if let cornorRadius {
             self.cornerRadius = cornorRadius
         }
 
-        self.stateDidChange()
+        self.setState(.normal, sendActions: false)
     }
     
     private var labelCreated: Bool = false
     weak var _label: Label! = nil
     func createLabel() {
-        let label = Label(text: "Button", font: .default, fontSize: 18, style: .bold, textColor: self.textColors[.normal] ?? .white)
+        let label = Label(text: kind == .momentaryPush ? "Button" : "Toggle", font: .default, fontSize: 18, style: .bold, textColor: self.textColors[.normal] ?? .white)
         label.centerXAnchor.constrain(to: self.centerXAnchor)
         label.centerYAnchor.constrain(to: self.centerYAnchor)
         label.widthAnchor.constrain(to: self.widthAnchor)
