57342fbb6f7542930fbf17d03578d1e125fa4e9d
STREGA <STREGA@users.noreply.github.com>
2025-11-25 09:21:20 -0500

    Refactor IDGenerator

--- Sources/GateEngine/Helpers/IDGenerator.swift
+++ Sources/GateEngine/Helpers/IDGenerator.swift
@@ -5,27 +5,30 @@
  * http://stregasgate.com
  */
 
+// A protocol to make sure all the possible variations have the same interface
+public protocol IDGeneratorProtocol: Sendable {
+    associatedtype T: BinaryInteger & Sendable
+    func generateID() -> T
+    init(startValue: T)
+}
+
 #if canImport(Atomics)
 public import Atomics
-#elseif canImport(Foundation.NSLock)
-import class Foundation.NSLock
-#endif
-
-#if canImport(Atomics)
-nonisolated public final class IDGenerator<T: AtomicInteger & Sendable> {
+public struct IDGenerator<T: AtomicInteger & Sendable>: IDGeneratorProtocol {
     var value: ManagedAtomic<T>
-
     public init(startValue: T = 0) {
         value = ManagedAtomic<T>(startValue)
     }
 
     public func generateID() -> T {
-        return value.loadThenWrappingIncrement(ordering: .sequentiallyConsistent)
+        return value.loadThenWrappingIncrement(ordering: .relaxed)
     }
 }
 #elseif canImport(Foundation.NSLock)
-nonisolated public final class IDGenerator<T: BinaryInteger & Sendable> {
-    var value: T = 0
+import class Foundation.NSLock
+public final class IDGenerator<T: BinaryInteger & Sendable>: IDGeneratorProtocol {
+    // isolation is managed with a lock, so this is safe
+    nonisolated(unsafe) var value: T = 0
     let lock = NSLock()
 
     public init(startValue: T = 0) {
@@ -33,18 +36,16 @@ nonisolated public final class IDGenerator<T: BinaryInteger & Sendable> {
     }
 
     public func generateID() -> T {
-        lock.lock()
-        let value = value
-        self.value += 1
-        defer {
-            lock.unlock()
+        return lock.withLock {
+            let value = value
+            self.value += 1
+            return value
         }
-        return value
     }
 }
 #else
-nonisolated public final class IDGenerator<T: BinaryInteger & Sendable> {
-    var value: T = 0
+public final class IDGenerator<T: BinaryInteger & Sendable>: IDGeneratorProtocol {
+    nonisolated(unsafe) var value: T = 0
 
     public init(startValue: T = 0) {
         self.value = startValue
