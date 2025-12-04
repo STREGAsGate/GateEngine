892fc1ffcbd0f6bd5218e8ef72b67d7e5c1645a2
STREGA <STREGA@users.noreply.github.com>
2025-12-04 15:24:18 -0500

    Replace with collection functionality

--- Sources/GateEngine/Physics/RawGeometry+Extensions.swift
+++ Sources/GateEngine/Physics/RawGeometry+Extensions.swift
@@ -7,55 +7,6 @@
 
 extension RawGeometry {
     public func generateCollisionTriangles<Attributes: CollisionAttributesGroup>(using attributesType: Attributes.Type = BasicCollisionAttributes.self) -> [CollisionTriangle] {
-        var positions: [Position3] = []
-        positions.reserveCapacity(self.vertices.count)
-        var uvs: [[Position2]] = Array(repeating: [], count: uvSets.count)
-        for uvSet in uvs.indices {
-            uvs[uvSet].reserveCapacity(positions.capacity)
-        }
-
-        for vertexIndex in self.vertices.indices {
-            let index = Int(vertexIndicies[vertexIndex])
-            let start3 = index * 3
-            let start2 = index * 2
-
-            for uvIndex in 0 ..< uvSets.count {
-                let uvSet = uvSets[uvIndex]
-                uvs[uvIndex].append(Position2(uvSet[start2], uvSet[start2 + 1]))
-            }
-
-            positions.append(
-                Position3(
-                    self.positions[start3],
-                    self.positions[start3 + 1],
-                    self.positions[start3 + 2]
-                )
-            )
-        }
-
-        func attributeUVs(forTiangle index: Int) -> CollisionAttributeUVs {
-            var triangleUVs: [CollisionAttributeUVs.TriangleUVs] = []
-            for uvIndex in 0 ..< uvSets.count {
-                triangleUVs.append(
-                    CollisionAttributeUVs.TriangleUVs(
-                        uv1: .init(uvs[uvIndex][index + 0]),
-                        uv2: .init(uvs[uvIndex][index + 1]),
-                        uv3: .init(uvs[uvIndex][index + 2])
-                    )
-                )
-            }
-            return CollisionAttributeUVs(uvSets: triangleUVs)
-        }
-
-        return stride(from: 0, to: positions.count, by: 3).map({
-            CollisionTriangle(
-                p1: positions[$0 + 0],
-                p2: positions[$0 + 1],
-                p3: positions[$0 + 2],
-                normal: nil,
-                using: attributesType,
-                with: attributeUVs(forTiangle: $0)
-            )
-        })
+        return self.map({CollisionTriangle($0, using: attributesType)})
     }
 }
