5fd1a0d5437c04a386a5a61878e6237e85fc2129
STREGA <STREGA@users.noreply.github.com>
2025-12-04 15:24:35 -0500

    Refactor

--- Sources/GateEngine/Physics/CollisionTriangle+Extensions.swift
+++ Sources/GateEngine/Physics/CollisionTriangle+Extensions.swift
@@ -5,7 +5,7 @@
  * http://stregasgate.com
  */
 
-extension CollisionTriangle {
+public extension CollisionTriangle {
     init<Attributes: CollisionAttributesGroup>(
         p1: Position3, p2: Position3, p3: Position3, normal: Direction3?,
         using attributesType: Attributes.Type = BasicCollisionAttributes.self,
@@ -21,21 +21,16 @@ extension CollisionTriangle {
     }
     
     init<Attributes: CollisionAttributesGroup>(_ triangle: Triangle, using attributesType: Attributes.Type = BasicCollisionAttributes.self) {
+        let collisionAttributeUVs = CollisionAttributeUVs(uvSets: [
+            .init(uv1: triangle.v1.uv1, uv2: triangle.v2.uv1, uv3: triangle.v3.uv1),
+            .init(uv1: triangle.v1.uv2, uv2: triangle.v2.uv2, uv3: triangle.v3.uv2),
+        ])
         self.init(
             p1: triangle.v1.position,
             p2: triangle.v2.position,
             p3: triangle.v3.position,
             normal: triangle.faceNormal,
-            attributes: Attributes(parsingUVs: triangle.collisionAttributeUVs)
+            attributes: Attributes(parsingUVs: collisionAttributeUVs)
         )
-    }
-}
-
-extension Triangle {
-    var collisionAttributeUVs: CollisionAttributeUVs {
-        return CollisionAttributeUVs(uvSets: [
-            .init(uv1: v1.uv1, uv2: v2.uv1, uv3: v3.uv1),
-            .init(uv1: v1.uv2, uv2: v2.uv2, uv3: v3.uv2),
-        ])
     }
 }
