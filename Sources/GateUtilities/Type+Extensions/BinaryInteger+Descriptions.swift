12f3a76176afa49265f88d958f00a29f6c1629b7
STREGA <STREGA@users.noreply.github.com>
2026-01-05 10:58:15 -0500

    Refactor into multiple variants

--- Sources/GateUtilities/Type+Extensions/BinaryInteger+Descriptions.swift
+++ Sources/GateUtilities/Type+Extensions/BinaryInteger+Descriptions.swift
@@ -6,17 +6,31 @@
  */
 
 public extension BinaryInteger {
-    /// The representation of this integer as a padded binary string `0b00000101`
-    var binaryDescription: String {
+    /// The representation of this integer as a padded binary string `00000101`
+    var binaryString: String {
         let string = String(self, radix: 2)
         let padding = String(repeating: "0", count: self.bitWidth - string.count)
-        return "0b" + padding + string
+        return padding + string
+    }
+    
+    /// The representation of this integer as a padded binary string `0b00000101`
+    /// This is the same as `binaryString` but with a prefix `0b`
+    var binaryDescription: String {
+        return "0b" + binaryString
+    }
+}
+
+public extension BinaryInteger {
+    /// The representation of this integer as a padded hex string `00FF`
+    var hexString: String {
+        let string = String(self, radix: 16)
+        let padding = String(repeating: "0", count: (MemoryLayout<Self>.size * 2) - string.count)
+        return padding + string.uppercased()
     }
     
     /// The representation of this integer as a padded hex string `0x00FF`
+    /// This is the same as `hesString` but with a prefix `0x`
     var hexDescription: String {
-        let string = String(self, radix: 16)
-        let padding = String(repeating: "0", count: (MemoryLayout<Self>.size * 2) - string.count)
-        return "0x" + padding + string.uppercased()
+        return "0x" + hexString
     }
 }
